405e826c184fc716c8f31e2c231b5b4779f3b7a6
Egemen <egemenkukrer@gmail.com>
2024-10-18 02:33:06 +0300

    req

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,11 +2,11 @@ require('@nomicfoundation/hardhat-toolbox');
 require('dotenv').config();
 
 module.exports = {
-  solidity: "0.8.27",
+  solidity: "0.8.26",
   networks: {
     sepolia: {
-      url: process.env.INFURA_URL, // Infura URL
-      accounts: [process.env.PRIVATE_KEY], // Özel anahtar
+      url: process.env.INFURA_URL, 
+      accounts: [process.env.PRIVATE_KEY], 
     },
   },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,19 +2,23 @@ const { ethers } = require('hardhat');
 require('dotenv').config();
 
 async function main() {
-    // Kontratın tanımını al
+    // FootballData kontratını oluştur
     const FootballData = await ethers.getContractFactory('FootballData');
+
+    console.log('Kontrat deploy ediliyor...');
 
     // Kontratı dağıt
     const footballData = await FootballData.deploy();
 
-    console.log('FootballData adres:', footballData.address);
+    // Dağıtım tamamlandıktan sonra, kontratın adresini yazdır
+    await footballData.waitForDeployment(); // Burada yeni yöntem kullanılıyor
+    console.log('FootballData kontratı başarıyla dağıtıldı. Adresi:', footballData.target);
 }
 
-// Hatanın yakalanması
+// Ana işlemi çalıştır
 main()
     .then(() => process.exit(0))
     .catch((error) => {
-        console.error(error);
+        console.error('Ana işlem sırasında hata:', error);
         process.exit(1);
     });
